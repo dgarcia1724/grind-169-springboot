8450a6b8d5cc35299cce16edb969052f4f0ddf97
Daniel Garcia <dannyg17017@gmail.com>
2024-12-14 11:44:54 -0800

    Fresh start

---
+++ src/main/java/danny/grind_169_backend/Grind169BackendApplication.java
@@ -0,0 +1,13 @@
+package danny.grind_169_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grind169BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grind169BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danny/grind_169_backend/Grind169BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danny.grind_169_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grind169BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
